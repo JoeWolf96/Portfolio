c0d9db84e769ca5de4b1e8b5a251582ce9885a54
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-06-14 18:40:07 +0100

    Updates

---
+++ precache-manifest.464cf867827c0cfbe64566488a656195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfc5ea03608ebdd781a5c0b0d8adc2d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ec73c0ae78dc94e930db",
+    "url": "/Portfolio/static/css/2.568d96fb.chunk.css"
+  },
+  {
+    "revision": "7d2222589528fb4af966",
+    "url": "/Portfolio/static/css/main.d8547519.chunk.css"
+  },
+  {
+    "revision": "ec73c0ae78dc94e930db",
+    "url": "/Portfolio/static/js/2.39ba8a80.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/Portfolio/static/js/2.39ba8a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2222589528fb4af966",
+    "url": "/Portfolio/static/js/main.d9d38790.chunk.js"
+  },
+  {
+    "revision": "90261a1ab73ac84bd242",
+    "url": "/Portfolio/static/js/runtime-main.1282757a.js"
+  }
+]);
